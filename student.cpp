e7151c33f974c14dee5d88bb877f7227b236cc1a
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-13 18:00:07 -0800

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,1 +1,22 @@
+#include "Student.h"
+#include <cstring>
 
+Student::Student(char* nName){
+
+  name = new char[20];
+  strcpy(name,nName);
+
+}
+
+
+Student::~Student(){
+
+  delete name;
+  
+}
+
+char* Student::getName(){
+
+  return name;
+
+}
