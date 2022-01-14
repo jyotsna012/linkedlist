693447296b012b8eb30e615bcd737b6bb6d22379
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-13 18:25:20 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "Student.h"
+#include "Node.h"
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+int main(){
+
+  cout << "Enter the name of the first student" << endl;
+  char name1[80] = "";
+  cin.get(name1,80);
+  cin.get()
+  Student* student1 = new Student(name1);
+  Node* head = new Node(student1);
+  
+  cout << "Enter the name of the second student" << endl;
+  char name2[80] = "";
+  cin.get(name2,80);
+  cin.get()
+  Student* student2 = new Student(name2);
+  Node* next = new Node(student2);
+  
+  head->setNext(next);
+  Node* cNode = head;
+  
+  while(0==0){
+
+    cout << (cNode->getStudent())->getName() << endl;
+    if(cNode->getNext()==NULL){
+
+      break;
+
+    }
+    cNode = cNode->getNext();
+    
+  }
+  return 0;
+  
+}
